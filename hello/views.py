14ab406f58080fc7abae5a972a8dc24285953656
Metoa toa <shimul929@gmail.com>
2018-11-16 01:17:48 +0600

    test

--- hello/views.py
+++ hello/views.py
@@ -3,10 +3,50 @@ from django.http import HttpResponse
 
 from .models import Greeting
 
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import pickle
+
+
+
+
+
+def sending(leadmail, body):  # mail sender function
+    fromaddr = "openislam88@gmail.com"
+
+    msg = MIMEMultipart()
+
+    msg['From'] = fromaddr
+    msg['To'] = leadmail
+    msg['Subject'] = ""
+
+    msg.attach(MIMEText(body, 'html'))
+
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(fromaddr, "rosaria08")
+    text = msg.as_string()
+    server.sendmail(fromaddr, leadmail, text)
+    server.quit()
+
+
+
+
+
+
+
+
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
+    leadmail='shimul929@gmail.com'
+    msg_1 = "helloooooooooooooooooo"
+    sending(leadmail, msg_1)
     return render(request, "index.html")
+
+    
+
 
 
 def db(request):
@@ -17,3 +57,5 @@ def db(request):
     greetings = Greeting.objects.all()
 
     return render(request, "db.html", {"greetings": greetings})
+
+
